3332b4111f2140121d5bdc80e0444b4290df0b36
= <=>
2022-01-22 22:04:26 +0530

    remove hardcoding used to test

--- helper.js
+++ helper.js
@@ -10,7 +10,6 @@ function getReturnAmount(stakeAmount, stakeRatio){
 function getRandomNumber(){
     const min = 1;
     const max = 5;
-    return 4;
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
